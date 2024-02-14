451ca12d4fc94ef4f4b0fe95455e38cd9c3f6596
Diego Silva <nascimentod322@gmail.com>
2024-02-14 10:30:42 -0300

    Adicionar arquivos de projeto.

---
+++ ConhecendoSplit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConhecendoSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funcionamento do Split: ");
+
+            string s = Console.ReadLine();
+
+            string[] vet = s.Split(',');
+            string A = vet[0];
+            string B = vet[1];
+            string C = vet[2];
+
+            string numeros = Console.ReadLine();
+
+            string[] conjuntos = s.Split(' ');
+            string zero = vet[0];
+            string um = vet[1];
+            string dois = vet[2];
+
+
+
+            Console.WriteLine(s);
+            Console.WriteLine(numeros);
+
+            Console.ReadLine();
+        }
+    }
+}
